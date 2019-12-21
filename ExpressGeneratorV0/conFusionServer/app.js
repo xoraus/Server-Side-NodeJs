a5c193ff222bfb15e46e9822e839cea2ed01af59
Sajjad Salaria~ <imsafay@gmail.com>
2019-12-21 16:05:00 +0530

    Express Generator REST API - rv01

--- ExpressGeneratorV0/conFusionServer/app.js
+++ ExpressGeneratorV0/conFusionServer/app.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// code round-v0
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
+
 var app = express();
 
 // view engine setup
@@ -19,8 +25,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// code-round-v0
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
