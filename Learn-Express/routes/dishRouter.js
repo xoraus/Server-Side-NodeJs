90cd7174bbbd0bd6b17e64baa7660f1927aaf275
Sajjad Salaria~ <imsafay@gmail.com>
2019-12-21 15:32:36 +0530

    express-generator-v00

--- Learn-Express/routes/dishRouter.js
+++ Learn-Express/routes/dishRouter.js
@@ -5,29 +5,24 @@ const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
-
 dishRouter.route('/')
-.all((req, res, next) => {
+.all((req,res,next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
+    res.setHeader('Content-Type', 'text/plain');
     next();
 })
-
-.get((req, res, next) => {
-    res.end('Will send all the available dishes to you!');
+.get((req,res,next) => {
+    res.end('Will send all the dishes to you!');
 })
-
 .post((req, res, next) => {
-    res.end('Will add the dish: '+ req.body.name + ' with details: ' +req.body.description);
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
 })
-
 .put((req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
-
 .delete((req, res, next) => {
-    res.end('deleting all the dishes from /dishes');
+    res.end('Deleting all dishes');
 });
 
 module.exports = dishRouter;
