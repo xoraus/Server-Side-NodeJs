5f2b916adf760eeeb1a4a6dfdd7b6311596ec063
Sajjad Salaria~ <imsafay@gmail.com>
2019-12-24 17:37:24 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- ExpressGeneratorV0/conFusionServer/app.js
+++ ExpressGeneratorV0/conFusionServer/app.js
@@ -13,6 +13,24 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 
+// week-2 code / added models folder
+
+
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
+
+
+
+
 var app = express();
 
 // view engine setup
