fb84179980296f897349decd25c3b7ee8f33184f
Sajjad Salaria~ <imsafay@gmail.com>
2019-12-30 11:48:47 +0530

    mongoose-population

---
+++ mongoose-Population/dishes.js
@@ -0,0 +1,20 @@
+
+
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
